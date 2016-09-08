d8ac8d2f72e356dcd95cd2acb3fa2dcd833bd696
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-09-08 17:55:45 +0200

    communicate with server using ajax

---
+++ app/assets/javascripts/markov_chainers.js
@@ -0,0 +1,44 @@
+function processForm() {
+  // debugger;
+  var input_text = $(document).find('#markov_chainer_input_text').val();
+  var order = $(document).find('#markov_chainer_order').val();
+  var n_sentences = $(document).find('#markov_chainer_n_sentences').val();
+
+  var data = JSON.stringify({
+    input_text: input_text,
+    order: order,
+    n_sentences: n_sentences
+  });
+
+  return data;
+};
+
+function submitForm(event) {
+  event.preventDefault();
+
+  var action = $(this).attr('action');
+  var method = $(this).attr('method');
+
+  var processedForm = processForm();
+
+  console.log(processedForm)
+
+  $.ajax({
+    type: method,
+    url: action,
+    data: processedForm,
+    contentType: 'application/json',
+    dataType: 'json'
+  }).done(function(data) {
+    console.log(data);
+    $('.markified-text').html("<p>" + data + "</p>");
+  }).fail(function(error) {
+    console.log(error)
+  });
+
+
+};
+
+$(document).ready(function() {
+  $('#new_markov_chainer').bind('submit', submitForm);
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root 'markov_chainers#index'
   post '/markov_chainers', to: 'markov_chainers#create'
-  get '/markov_chainers/show', to: 'markov_chainers#show'
 end

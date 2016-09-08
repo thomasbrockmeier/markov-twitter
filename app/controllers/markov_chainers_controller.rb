f4903342307e47a0846507b476d1a4ce5978fd7e
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-09-08 13:03:50 +0200

    save params to session

--- app/controllers/markov_chainers_controller.rb
+++ app/controllers/markov_chainers_controller.rb
@@ -2,4 +2,28 @@ class MarkovChainersController < ApplicationController
   def index
     @markov ||= MarkovChainer.new
   end
+
+  def create
+    @markov = MarkovChainer.new(markov_chainer_params)
+
+    session[:input_text] = markov_chainer_params[:input_text]
+    session[:order] = markov_chainer_params[:order]
+    session[:n_sentences] = markov_chainer_params[:n_sentences]
+
+    redirect_to root_path
+  end
+
+  def show(markov)
+    @markov
+  end
+
+  private
+
+  def markov_chainer_params
+    params.require(:markov_chainer).permit(
+      :input_text,
+      :order,
+      :n_sentences
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'markov_chainers#index'
+  post '/markov_chainers', to: 'markov_chainers#create'
+  get '/markov_chainers/show', to: 'markov_chainers#show'
 end

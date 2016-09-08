8d87c7f2544ee69dc18b4bb54afc6a4ec801a102
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-09-08 11:53:05 +0200

    fix db error

--- db/migrate/20160908094930_create_markov_chainers.rb
+++ db/migrate/20160908094930_create_markov_chainers.rb
@@ -1,8 +1,8 @@
 class CreateMarkovChainers < ActiveRecord::Migration[5.0]
   def change
     create_table :markov_chainers do |t|
-      t.text, :input-text
-      t.integer, :order
+      t.text :input_text
+      t.integer :order
       t.integer :n_sentences
 
       t.timestamps

e955afd5d5e402a23f3dba45b93c441e4058cefc
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-09-09 12:18:38 +0200

    create and integrate twitter client with markov chain

--- app/controllers/markov_chainers_controller.rb
+++ app/controllers/markov_chainers_controller.rb
@@ -12,10 +12,10 @@ class MarkovChainersController < ApplicationController
 
     @markov = MarkovChainer.new(markov_chainer_params)
 
-    p @markov.process_input
+    p @markov.process_twitter_account
 
     respond_to do |format|
-      if response_string = @markov.process_input
+      if response_string = @markov.process_twitter_account
         format.html {
           # flash[:input_text] = response_string
           # redirect_to root_path
--- app/models/markov_chainer.rb
+++ app/models/markov_chainer.rb
@@ -1,3 +1,5 @@
+require 'TwitterClient'
+
 class MarkovChainer < ApplicationRecord
   after_initialize :init
 
@@ -11,5 +13,35 @@ class MarkovChainer < ApplicationRecord
     markov = MarkyMarkov::Dictionary.new('dictionary', self.order)
     markov.parse_string(self.input_text)
     markov.generate_n_sentences n_sentences
+  end
+
+  def process_twitter_account
+    tweets = retrieve_tweets(self.input_text)
+    text = extract_text(tweets)
+    filtered_text = filter_text(text)
+
+    markov = MarkyMarkov::Dictionary.new('dictionary', self.order)
+    markov.parse_string(filtered_text)
+    markov.generate_n_sentences n_sentences
+
+  end
+
+  def retrieve_tweets(twitter_handle)
+    c = TwitterClient.new.client
+    tweets = c.user_timeline(twitter_handle, {count: 200})
+  end
+
+  def extract_text(tweets)
+    text = ''
+    tweets.each do |t|
+      text << (t::text) + ' '
+    end
+
+    text
+  end
+
+  def filter_text(text)
+    # Remove hashtags, handlers, and URLs (#hashtag, @kanyewest, http(s)://...)
+     processed_text = text.gsub(/(#\S*|@\S*|http(|s):\/\/\S*|www.\S*)/i, '')
   end
 end

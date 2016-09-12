1c0daf70791f41d38dee55123879bc90a5140a32
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-09-12 13:51:36 +0200

    refactor code

--- app/models/markov_chainer.rb
+++ app/models/markov_chainer.rb
@@ -31,6 +31,7 @@ class MarkovChainer < ApplicationRecord
 
 
   # Recursive call to Twitter API
+  # Source: https://github.com/sferik/twitter/blob/master/examples/AllTweets.md
   def collect_with_max_id(collection=[], max_id=nil, &block)
     response = yield(max_id)
     collection += response
@@ -48,7 +49,6 @@ class MarkovChainer < ApplicationRecord
 
 
   # Helper functions
-
   def extract_text(tweets)
     text = ''
     tweets.each do |t|
@@ -62,28 +62,30 @@ class MarkovChainer < ApplicationRecord
     # Remove hashtags, handlers, and URLs (#hashtag, @kanyewest, http(s)://..., www....)
     text.gsub(/(#\S*|@\S*|http(|s):\/\/\S*|www.\S*)/i, '')
   end
-
-  def retrieve_tweets(twitter_handle)
-      @c.user_timeline(twitter_handle, {count: 200})
-  end
-
-  def verify_account(twitter_handle)
-    begin
-      p @c.user(twitter_handle)
-      [true]
-    rescue Twitter::Error => e
-      p "Error: #{e}"
-      [false, e]
-    end
-  end
-
-
-
-
-  # Used to process whatever text is enterend on the view. Used for testing puropses
-  def process_input
-    markov = MarkyMarkov::Dictionary.new('dictionary', self.order)
-    markov.parse_string(self.input_text)
-    markov.generate_n_sentences n_sentences
-  end
 end
+
+  # # Deprecated helper functions
+  # def retrieve_tweets(twitter_handle)
+  #     @c.user_timeline(twitter_handle, {count: 200})
+  # end
+  #
+  # def verify_account(twitter_handle)
+  #   begin
+  #     p @c.user(twitter_handle)
+  #     [true]
+  #   rescue Twitter::Error => e
+  #     p "Error: #{e}"
+  #     [false, e]
+  #   end
+  # end
+
+
+
+#   # Testing functions
+#   # Used to process whatever text is enterend on the view. Used for testing
+#   # purposes
+#   def process_input
+#     markov = MarkyMarkov::Dictionary.new('dictionary', self.order)
+#     markov.parse_string(self.input_text)
+#     markov.generate_n_sentences n_sentences
+#   end
